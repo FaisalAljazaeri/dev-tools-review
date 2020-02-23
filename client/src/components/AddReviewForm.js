72d2896683a04ed5948df9f59498f80cbdd4c90f
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-02-23 15:41:03 +0300

    Add Child Component To AddReviewModal and Move Logic To It From Parent

---
+++ client/src/components/AddReviewForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+
+class AddReviewForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            itemName: "",
+            content: "",
+            itemImgSrc: "",
+            isRecommended: false
+        };
+    }
+
+    changehandler = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    submitHandler = e => {
+        e.preventDefault();
+        this.props.addReview(this.state);
+    };
+
+    render() {
+        return (
+            <Form onSubmit={this.submitHandler}>
+                <FormGroup>
+                    <Label>Item Name</Label>
+                    <Input
+                        type="text"
+                        name="itemName"
+                        placeholder="item name"
+                        onChange={this.changehandler}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Review Content</Label>
+                    <Input
+                        type="textarea"
+                        name="content"
+                        placeholder="Review Content"
+                        onChange={this.changehandler}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Image Src</Label>
+                    <Input
+                        type="text"
+                        name="itemImgSrc"
+                        placeholder="Image Src"
+                        onChange={this.changehandler}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Do You Recommend It?</Label>
+                    <Input
+                        type="select"
+                        name="isRecommended"
+                        onChange={this.changehandler}
+                    >
+                        <option value={false}>No</option>
+                        <option value={true}>Yes</option>
+                    </Input>
+                </FormGroup>
+                <Button type="submit" color="primary">
+                    Submit
+                </Button>{" "}
+                <Button color="secondary" onClick={this.props.closeModal}>
+                    Cancel
+                </Button>
+            </Form>
+        );
+    }
+}
+
+export default AddReviewForm;
