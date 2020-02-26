7b5ec1a25df0566b6e56a09003151e32920b1b7a
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-02-26 16:05:01 +0300

    Updates

---
+++ precache-manifest.4fa2e0175f1f99a01e9994b0cbe42b92.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a92e7bbeb98762edae082340e383608",
+    "url": "/dev-tools-review/index.html"
+  },
+  {
+    "revision": "b1afaef5c5efd3870861",
+    "url": "/dev-tools-review/static/css/2.2b1e90c7.chunk.css"
+  },
+  {
+    "revision": "f05a3021d06cd19a1604",
+    "url": "/dev-tools-review/static/css/main.6b054486.chunk.css"
+  },
+  {
+    "revision": "b1afaef5c5efd3870861",
+    "url": "/dev-tools-review/static/js/2.00309c14.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/dev-tools-review/static/js/2.00309c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05a3021d06cd19a1604",
+    "url": "/dev-tools-review/static/js/main.2d7d5dc4.chunk.js"
+  },
+  {
+    "revision": "e9989824590a6ffbb957",
+    "url": "/dev-tools-review/static/js/runtime-main.e823ba2f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/dev-tools-review/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/dev-tools-review/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/dev-tools-review/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dev-tools-review/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/dev-tools-review/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
